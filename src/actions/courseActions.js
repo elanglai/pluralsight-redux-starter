4efe7df3e9005da800eaa140cd81da39865f9b88
Eric Langlais <elanglai@teksystems.com>
2018-08-27 14:34:18 -0400

    Create Save Course Trunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,6 +4,12 @@ import courseApi from '../api/mockCourseApi';
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
 }
+export function updateCourseSuccess(courses) {
+  return { type: types.UPDATE_COURSE_SUCCESS, courses};
+}
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, courses};
+}
 
 
 export function loadCourses() {
@@ -11,6 +17,18 @@ export function loadCourses() {
     // getAllCourses returns a promise.
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  // The getState can be used to access the redux store directly
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      // Either update or create a course
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) : dispatch(createCourseSucess(savedCourse));
     }).catch(error => {
       throw(error);
     });
